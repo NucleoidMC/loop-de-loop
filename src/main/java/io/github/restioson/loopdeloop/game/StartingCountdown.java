2741d0d36877a8e22d063cb3a5991b27dc3df6b9
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-12-23 11:50:16 -0500

    Update minigame to Minecraft 1.19.3

--- src/main/java/io/github/restioson/loopdeloop/game/StartingCountdown.java
+++ src/main/java/io/github/restioson/loopdeloop/game/StartingCountdown.java
@@ -42,10 +42,10 @@ public final class StartingCountdown {
         if ((this.startTime - time) % 20 == 0) {
             if (sec > 0) {
                 players.showTitle(Text.literal(Integer.toString(sec)).formatted(Formatting.BOLD), 1, 5, 3);
-                players.playSound(SoundEvents.BLOCK_NOTE_BLOCK_HARP, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                players.playSound(SoundEvents.BLOCK_NOTE_BLOCK_HARP.value(), SoundCategory.PLAYERS, 1.0F, 1.0F);
             } else {
                 players.showTitle(Text.literal("Go!").formatted(Formatting.BOLD), 1, 5, 3);
-                players.playSound(SoundEvents.BLOCK_NOTE_BLOCK_HARP, SoundCategory.PLAYERS, 1.0F, 2.0F);
+                players.playSound(SoundEvents.BLOCK_NOTE_BLOCK_HARP.value(), SoundCategory.PLAYERS, 1.0F, 2.0F);
                 return true;
             }
         }
